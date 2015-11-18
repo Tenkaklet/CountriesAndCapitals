8a93abf3507608577d76010a17229a68cd5f419c
Tenkaklet <tenkaklet@gmail.com>
2015-11-18 23:32:51 +0100

    finished structure, lets begin

--- app/js/master.js
+++ app/js/master.js
@@ -1,4 +1,18 @@
 // Master file of app initiation and configuration
 
 var countriesApp = angular.module('countriesApp',['ngAnimate', 'ngRoute']);
-countriesApp.config();
+countriesApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'HomeController'
+    })
+    .when('/countDetails', {
+        templateUrl: 'pages/countDetails.html',
+        controller: 'DetailsController'
+    })
+    .when('/countList', {
+        templateUrl: 'pages/countList.html',
+        controller: 'ListController'
+    })
+    .otherwise('/');
+});
