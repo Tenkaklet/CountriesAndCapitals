d9b18a8b81295564c29f64a69c1643e61ce99d73
Tenkaklet <tenkaklet@gmail.com>
2015-11-21 19:59:54 +0100

    reworked for help

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,16 +14,33 @@ countriesApp.controller('CountryController', ['$scope', '$http','$q', '$timeout'
 
 }]);
 countriesApp.controller('CapitalController' , ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
+    $scope.$on('$routeChangeSuccess', function () {
+        $scope.population = $routeParams.population;
+        console.log($routeParams.population);
+    });
     // GEt country code.
-    $scope.country = $routeParams.country;
-    $scope.countryCode = $routeParams.countryCode;
-    $scope.continent = $routeParams.continent;
-    $routeParams = {
-        country: $scope.country,
-        population: $scope.population,
-        continent: $scope.continent,
-    };
-    console.log($routeParams);
+   $scope.country = $routeParams.country;
+   $scope.countryCode = $routeParams.countryCode;
+   $scope.continent = $routeParams.continent;
+   $scope.countryId = $routeParams.countryId;
+   console.log($scope.countryId);
+   var url = 'http://api.geonames.org/search?username=tenkaklet&type=json&q=' + $routeParams.country + '&isNameRequired=true&name_equals=' + $routeParams.country + '&country=' + $routeParams.countryCode;
+   console.log(url);
+   $http.get(url, {})
+       .then(function (response){
+           console.log(response);
+           var short = response.data.geonames[0];
+           $routeParams = {
+               country: $scope.country,
+               population: short.population,
+               fcode: short.fcodeName
+           };
+
+           $scope.population = $routeParams.population;
+           $scope.continent = $routeParams.continent;
+           console.log($routeParams);
+       });
+
     // $http.get(searchURL, {searchParams: searchParams})
     // .then(function (response) {
     //     $scope.countries = response.data.geonames;
