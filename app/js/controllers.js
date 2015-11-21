170e7fb59d383fbddf0a11bd9d8fae55cc3e53e7
Tenkaklet <tenkaklet@gmail.com>
2015-11-21 13:17:46 +0100

    as object

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,21 +14,10 @@ countriesApp.controller('CountryController', ['$scope', '$http','$q', '$timeout'
 
 }]);
 countriesApp.controller('CapitalController' , ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
-    var searchURL = 'http://api.geonames.org/searchJSON?&username=tenkaklet';
-    var searchParams = {
-        username: 'tenkaklet',
-        type: 'json',
-        q: $scope.country,
-        isNameRequired: true,
-        name_equals: $scope.country,
-        country: $routeParams.countryCode,
-        population: $routeParams.population,
-    };
     // GEt country code.
     $scope.country = $routeParams.country;
     $scope.countryCode = $routeParams.countryCode;
     $scope.continent = $routeParams.continent;
-    $scope.population = $routeParams.population;
     $routeParams = {
         country: $scope.country,
         population: $scope.population,
