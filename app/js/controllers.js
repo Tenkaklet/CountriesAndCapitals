22858eb04e1bd2b9fe1b68de0e3048cac7c67925
Tenkaklet <tenkaklet@gmail.com>
2015-11-22 11:43:39 +0100

    finished, finally

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,48 +10,40 @@ countriesApp.controller('CountryController', ['$scope', '$http','$q', '$timeout'
         $http.get(geoURL, {params:params})
         .then(function(response){
             $scope.countries = response.data.geonames;
-            console.log(response);
         });
 
 }]);
-countriesApp.controller('CapitalController' , ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
-    $scope.$on('$routeChangeSuccess', function () {
-        $scope.population = $routeParams.population;
-        // console.log($routeParams.population);
-    });
-    // GEt country code.
-    // good with "global vairables for get"
-   $scope.country = $routeParams.country;
-   $scope.countryCode = $routeParams.countryCode;
-   $scope.continent = $routeParams.continent;
-   $scope.countryId = $routeParams.countryId;
-   $scope.population = $routeParams.population;
-   $scope.area = $routeParams.area;
-   $scope.capital = $routeParams.capital;
-   // console.log($scope.countryId);
-   var url = 'http://api.geonames.org/search?username=tenkaklet&type=json&q=' + $routeParams.country + '&isNameRequired=true&name_equals=' + $routeParams.country + '&country=' + $routeParams.countryCode;
-   // console.log(url);
-   $http.get(url, {})
-       .then(function (response){
-           $scope.population = response.data.geonames[0].population;
-        //    console.log(response);
-           var geoNameID = response.data.geonames[0].geonameId;
-           var neighbourUrl = 'http://api.geonames.org/neighbours?username=tenkaklet&type=json&geonameId=' + geoNameID;
-           $http.get(neighbourUrl, {})
-           .then(function (response) {
-               console.log(response);
-               $scope.neighbours = response.data.geonames;
-           });
+countriesApp.controller('CapitalController' , ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams ){
+    $scope.country = $routeParams.country;
+    $scope.countryCode = $routeParams.countryCode;
+    $scope.continent = $routeParams.continent;
+    $scope.countryId = $routeParams.countryId;
+    $scope.population = $routeParams.population;
+    $scope.area = $routeParams.area;
+    $scope.capital = $routeParams.capital;
+    $scope.capPop = $routeParams.capPop;
+    var url = 'http://api.geonames.org/search?username=tenkaklet&type=json&q=' + $routeParams.country + '&isNameRequired=true&name_equals=' + $routeParams.country + '&country=' + $routeParams.countryCode;
+    $http.get(url, {})
+        .then(function (response){
+            $scope.population = response.data.geonames[0].population;
+            var geoNameID = response.data.geonames[0].geonameId;
+            var neighbourUrl = 'http://api.geonames.org/neighbours?username=tenkaklet&type=json&geonameId=' + geoNameID;
+            $http.get(neighbourUrl, {})
+            .then(function (response) {
+                console.log(response);
+                $scope.neighbours = response.data.geonames;
+                $scope.neighbours.area = $routeParams.area;
+                $scope.neighbours.capital = $routeParams.capital;
+                $scope.numOfNeighbors = response.data.totalResultsCount;
+                console.log($routeParams.capital);
+            });
 
-       });
+        });
+        var capURL = 'http://api.geonames.org/searchJSON?formatted=true&username=tenkaklet&q=capital&country=' + $routeParams.countryCode;
+        $http.get(capURL, {})
+         .then(function (response) {
 
+             $scope.capPop = response.data.geonames[0].population;
+         });
 
-    // $http.get(searchURL, {searchParams: searchParams})
-    // .then(function (response) {
-    //     $scope.countries = response.data.geonames;
-    //     // console.log(dope);
-    //     // console.log(response.data.geonames);
-    // });
 }]);
-
-//You could say $routeParams is the $scope LINK between controllers.......
